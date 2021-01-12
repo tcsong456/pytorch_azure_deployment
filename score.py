159c8f32c2328a723e98763fcf44765de4df4e43
Andy Song <congxisong@hotmail.com>
2021-01-12 19:39:00 +0000

    dev 17

--- score.py
+++ score.py
@@ -44,10 +44,10 @@ def run(raw_data):
     df = pd.DataFrame(raw_data['data'],columns=['split','class','image_id'])
     ws = Workspace.get(name='aml-workspace',
                        resource_group='aml-resource-group',
-                       subscription='64c727c2-4f98-4ef1-a45f-09eb33c1bd59')
+                       subscription_id='64c727c2-4f98-4ef1-a45f-09eb33c1bd59')
     datastore = ws.get_default_datastore()
     dataset = Dataset.File.from_files(path=(datastore,'pytorch'))
-    df = dataset.download('.',overwrite=True)
+    dataset.download('.',overwrite=True)
     if not os.path.exists('train') or not os.path.exists('val'):
         raise FileNotFoundError('train or val or both are not found on local drive')
     
